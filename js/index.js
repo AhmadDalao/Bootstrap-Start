5ca9ee6b4ffecc5e1e9552ba52e06169446c7708
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-05-28 05:57:53 +0300

    nothing

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ let navbarList = document.querySelector(".navbar-list");
 function myFunction() {
     if (navbarList.className === "navbar-list") {
         navbarList.classList.toggle("active");
+        // navbarList.style.transition = "all .5s easy-in-out";
     } else {
         navbarList.classList.toggle("active");
+        // navbarList.style.transition = "all .5s easy-in-out";
 
     }
 
